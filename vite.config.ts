df9e469d27501fece54869885e0dd964576f935d
samson <titussamson007@gmail.com>
2023-11-08 11:32:39 +0100

    second front

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,21 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Vue from '@vueuse/core';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  optimizeDeps: {
+    include: ['@vueuse/core'], // Assurez-vous d'inclure @vueuse/core ici
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
+
+
